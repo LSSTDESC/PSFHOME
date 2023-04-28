f2b363d332d35ab0c54f0dbca461e3af215032a6
Tianqing Zhang <ztq1996@live.com>
2023-04-27 21:59:03 -0400

    add raw moments measurement, and conversion from raw to standardized

--- psfhome/moments.py
+++ psfhome/moments.py
@@ -325,6 +325,7 @@ class shapeletXmoment:
 
         return pq_except
 
+
 def get_all_moments_fast(image, pqlist):
     
     """
@@ -376,4 +377,84 @@ def get_all_moments_fast(image, pqlist):
         
     t3 = time.time()        
     return np.array(results_list)
+
+
+def get_all_moments_raw(image,pqlist):
+    
+    results_list = []
+    
+    image_array = image.array
+    
+    y, x = mgrid[:image_array.shape[0],:image_array.shape[1]]+1
+    psfresults = galsim.hsm.FindAdaptiveMom(image)
+    M = np.zeros((2,2))
+    e1 = psfresults.observed_shape.e1
+    e2 = psfresults.observed_shape.e2
+    sigma4 = psfresults.moments_sigma**4
+    c = (1+e1)/(1-e1)
+    M[1][1] = np.sqrt(sigma4/(c-0.25*e2**2*(1+c)**2))
+    M[0][0] = c*M[1][1]
+    M[0][1] = 0.5*e2*(M[1][1]+M[0][0])
+    M[1][0] = M[0][1]
+    
+    
+    pos = np.array([x-psfresults.moments_centroid.x, y-psfresults.moments_centroid.y])
+    inv_M = np.linalg.inv(M)
+    sqrt_inv_M = alg.sqrtm(inv_M)
+    
+    std_pos = np.einsum('ij,jqp->iqp',sqrt_inv_M,pos)
+    weight = np.exp(-0.5* np.einsum('ijk,ijk->jk',std_pos,std_pos ))
+
+    pos_x, pos_y = pos[1],pos[0]
+    normalization = sum(image_array*weight)
+    image_weight = weight*image_array
+    for tup in pqlist:
+        p = tup[0]
+        q = tup[1]
+        
+        if q+p==2:
+            if p==2:
+                this_moment = e1
+            elif q==2:
+                this_moment = e2
+            else:
+                this_moment = psfresults.moments_sigma
+        else:
+            this_moment = sum(pos_x**p*pos_y**q*image_weight)/normalization
+        results_list.append(this_moment)
+        
+    return np.array(results_list)
+        
+
+def standard_from_raw(e1,e2,sigma,m40,m31,m22,m13,m04):
+    
+    """
+    Convert raw moments to standardized moments
+    m40,m31,m22,m13,m04 are raw fourth moments defined in the cartesian coordinate 
+    """
+    
+    sigma4 = sigma**4
+    c = (1 + e1) / (1 - e1)
+    myy = np.sqrt(sigma4 / (c - 0.25 * e2**2 * (1 + c) ** 2))
+    mxx = c * myy
+    mxy = 0.5 * e2 * (myy + mxx)
+    
+    d = mxx*myy-mxy**2
+    zeta = d*(mxx+myy+2*np.sqrt(d))
+    
+    a = (myy + np.sqrt(d))/np.sqrt(zeta)
+    b = -mxy/np.sqrt(zeta)
+    c = (mxx+np.sqrt(d))/np.sqrt(zeta)
+        
+    stan_m40 = a**4*m40 + 4*a**3*b*m31 + 6*a**2*b**2*m22 + 4*a*b**3*m13 + b**4*m04
+    stan_m31 = (a**3*b)*m40 + (a**3*c+3*a**2*b**2)*m31 + (3*a**2*b*c+3*a*b**3)*m22 + (3*a*b**2*c+b**4)*m13 + b**3*c*m04
+    stan_m22 = a**2*b**2*m40 + (2*a**2*b*c+2*a*b**3)*m31 + (a**2*c**2+4*a*b**2*c+b**4)*m22 + (2*a*b*c**2+2*b**3*c)*m13+ b**2*c**2*m04
+    stan_m13 = a*b**3*m40 + (3*a*b**2*c+b**4)*m31 + (3*a*b*c**2+3*b**3*c)*m22 + (a*c**3+3*b**2*c**2)*m13 + b*c**3*m04
+    stan_m04 = b**4*m40 + 4*b**3*c*m31 + 6*b**2*c**2*m22 + 4*b*c**3*m13 + c**4*m04
+
+    return stan_m40, stan_m31, stan_m22, stan_m13, stan_m04
+
+
+
+
         
